e5324754824ccaaab7220e8f849c1fa45dc6c8b2
desttruptor <41233120+desttruptor@users.noreply.github.com>
2021-03-27 13:52:07 +0300

    first valuable commit

--- app/src/main/java/me/podlesnykh/travelhackmvp/presentation/MainActivity.kt
+++ app/src/main/java/me/podlesnykh/travelhackmvp/presentation/MainActivity.kt
@@ -1,7 +1,8 @@
-package me.podlesnykh.travelhackmvp
+package me.podlesnykh.travelhackmvp.presentation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import me.podlesnykh.travelhackmvp.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
---
+++ app/src/main/java/me/podlesnykh/travelhackmvp/services/UserLocationService.kt
@@ -0,0 +1,12 @@
+package me.podlesnykh.travelhackmvp.services
+
+import android.app.Service
+import android.content.Intent
+import android.os.IBinder
+
+class UserLocationService : Service() {
+
+    override fun onBind(intent: Intent): IBinder {
+        TODO("Return the communication channel to the service.")
+    }
+}
